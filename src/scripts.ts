287eb492e4fe61ebe7db47fd2cb073278403d3c6
Knazat <rihk.karlauskis@gmail.com>
2022-04-13 21:41:15 +0300

    pedejas 3 pogas

--- src/scripts.ts
+++ src/scripts.ts
@@ -45,7 +45,7 @@ console.log(buttonEl3);
 
 
 const boxEl = document.querySelectorAll(".js-box");
-const buttonEl4 = document.querySelectorAll(".js-button")
+const buttonEl4 = document.querySelectorAll<HTMLButtonElement>(".js-button")
 
 buttonEl4.forEach((button: HTMLButtonElement) => {
     button.addEventListener("click", () => {
@@ -57,6 +57,118 @@ buttonEl4.forEach((button: HTMLButtonElement) => {
 })
 
 console.log(boxEl, buttonEl4)
+
+
+//BIG MD SAKAS SEIT
+
+//pirmas pogas efekts
+const box1 = document.querySelector<HTMLDivElement>('.js-box-1');
+const buttonToYellow = document.querySelector<HTMLButtonElement>('.js-buttonToYellow');
+
+buttonToYellow.addEventListener('click', () => 
+box1.classList.toggle('yellowBackground')   
+);
+
+//otras pogas efekts
+const  buttonToSuccess = document.querySelector<HTMLDivElement>('.js-buttonToSuccess');
+const box2 = document.querySelector<HTMLDivElement>('.js-box-2');
+
+buttonToSuccess.addEventListener('click', () => {
+  if(box2.innerText === 'FAIL'){
+    box2.innerText = 'SUCCESS'
+  } else{box2.innerText = 'FAIL'}  
+  });
+
+//tresas pogas efekts
+const buttonToInvisible = document.querySelector<HTMLButtonElement>('.js-buttonToInvisible');
+const box3 = document.querySelector<HTMLDivElement>('.js-box-3');
+
+buttonToInvisible.addEventListener('click', () => 
+box3.classList.add('invisibleBox')
+);
+
+//ceturtas pogas efekts
+const buttonToggleInvisible = document.querySelector<HTMLButtonElement>('.js-buttonToggleInvisible');
+const box4 = document.querySelector<HTMLDivElement>('.js-box-4');
+
+buttonToggleInvisible.addEventListener('click', () => 
+box4.classList.toggle('invisibleBox')
+);
+
+//piektas pogas efekts 
+const buttonSwitchColors = document.querySelector<HTMLButtonElement>('.js-buttonSwitchColors')
+const box5 = document.querySelector<HTMLDivElement>('.js-box-5')
+
+const box5Colors = ['yellow', 'orange', 'red', 'purple', 'blue']
+let counter = -1 
+
+buttonSwitchColors.addEventListener('click', () => {
+
+counter += 1;
+box5.style.backgroundColor = box5Colors[counter];
+if(box5Colors.length === counter+1){ counter=-1 };
+
+//mans sakotnejais atradums
+    // const x = Math.floor(Math.random() * 256);
+    // const y = Math.floor(Math.random() * 256);
+    // const z = Math.floor(Math.random() * 256);
+
+    //  box5.style.backgroundColor = "rgb(" + x + "," + y + "," + z + ")";
+
+});
+
+
+//sestas pogas efekts
+const buttonTimer = document.querySelector<HTMLButtonElement>('.js-buttonTimer');
+const box6 = document.querySelector<HTMLDivElement>('.js-box-6');
+
+
+// const numbers = ['0','1','2','3','4','5','6','7','8','9','10']
+let sekond =  0
+
+buttonTimer.addEventListener('click', () => {
+
+
+  const stopWatch = setInterval( () => {
+    sekond += 1 ;
+    box6.innerText = sekond.toString()
+    if(box6.innerText === '10'){ clearInterval(stopWatch), sekond = 0 }
+  
+ },300);  
+});
+
+//septitas pogas efekts
+const buttonAllBoxesToBlack = document.querySelector<HTMLButtonElement>('.js-buttonAllBoxesToBlack')
+const allBoxesToBlack = document.querySelectorAll<HTMLDivElement>('.js-all-boxes')
+const bodyToBlack = document.querySelector<HTMLBodyElement>('.js-bodyToBlack') 
+// variants bez toggle
+// buttonAllBoxesToBlack.addEventListener('click',() =>
+// allBoxesToBlack.forEach((boks: HTMLDivElement) => {
+//   boks.style.backgroundColor = 'black';
+// })
+// )
+
+buttonAllBoxesToBlack.addEventListener('click',() => 
+
+  // bodyToBlack.classList.toggle('toBlack')
+  allBoxesToBlack.forEach((boks: HTMLDivElement) => {
+    boks.classList.toggle('all-to-blue'); 
+  }))
+
+
+//hover uz pirmas kastes
+box1.addEventListener('mouseenter', () => 
+box1.style.backgroundColor = 'red'
+)
+box1.addEventListener('mouseleave', () => 
+box1.style.backgroundColor = '#1FC2AE'
+)
+
+
+
+
+
+
 
 
 
